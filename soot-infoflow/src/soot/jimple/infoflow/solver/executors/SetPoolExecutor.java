b356d58ba2d9148460dbe68064a13f8fc137414e
Marc Miltenberger <Marc.Miltenberger@sit.fraunhofer.de>
2025-12-06 23:20:00 +0100

    Test whether there is a duplicate

--- soot-infoflow/src/soot/jimple/infoflow/solver/executors/SetPoolExecutor.java
+++ soot-infoflow/src/soot/jimple/infoflow/solver/executors/SetPoolExecutor.java
@@ -15,40 +15,42 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class SetPoolExecutor extends InterruptableExecutor {
-	
+
 	protected Set<Runnable> waiting = Collections.newSetFromMap(new ConcurrentHashMap<Runnable, Boolean>());
-	
-	public SetPoolExecutor(int corePoolSize, int maximumPoolSize,
-			long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+
+	public SetPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
+			BlockingQueue<Runnable> workQueue) {
 		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
 	}
-	
+
 	@Override
 	public void execute(Runnable command) {
 		// Make sure that we don't schedule a task for execution that is already
 		// in the queue
 		if (waiting.add(command))
 			super.execute(command);
+		else
+			throw new RuntimeException("Double");
 	}
-	
+
 	@Override
 	protected void afterExecute(Runnable r, Throwable t) {
-		waiting.remove(r);
+		//waiting.remove(r);
 		super.afterExecute(r, t);
 	}
-	
+
 	@Override
 	public void interrupt() {
 		super.interrupt();
 		this.waiting.clear();
 	}
-	
+
 	@Override
 	public void shutdown() {
 		super.shutdown();
 		this.waiting.clear();
 	}
-	
+
 	@Override
 	public List<Runnable> shutdownNow() {
 		List<Runnable> tasks = super.shutdownNow();
